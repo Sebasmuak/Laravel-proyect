85104505b63cdb1ee0aff0bf298cfe8b2e371f8c
sebastian rubio <sebastianrubiotorres5@gmail.com>
2025-09-01 14:50:26 -0500

    feat: agrega ruta/ prueba para verificacion

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/prueba', function () {
+    return 'Hola, esta es una ruta de prueba desde feature/prueba';
+});
